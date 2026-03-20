8bd217d3f55a878c22c5579f1466c76a6e377f18
Joshua Poro <porojoshua@iMac.lan>
2026-03-20 13:32:40 -0500

    safe supabase test in client file

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,3 +5,9 @@ export const supabase = createClient(
   "https://swxhwdyszgmjznvhmavq.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3eGh3ZHlzemdtanpudmhtYXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5Mzc5OTcsImV4cCI6MjA4OTUxMzk5N30.nd1Vzmkhi0gklchYnu6OmW4C_LZ_CkS1_G7wQ2SPGW4"
 )
+async function test() {
+  const { data, error } = await supabase.from("sermons").select("*")
+  console.log("SUPABASE TEST:", data, error)
+}
+
+test()
